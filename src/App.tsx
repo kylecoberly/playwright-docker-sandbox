8bcd44835f6f1fa39e1e6b164e2302a1b126411f
Kyle Coberly <kyle.coberly@gmail.com>
2024-04-12 01:38:15 -0600

    App works

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,8 @@
 export default function App() {
-  return <div class="App"></div>;
+  return (
+    <div className="App">
+      <h1>Some App</h1>
+      <iframe src="http://localhost:8080">Iframe error</iframe>
+    </div>
+  );
 }
