11af4fc3c50d1579a99d2bbc8f085a3214556b33
Kyle Coberly <kyle.coberly@gmail.com>
2024-04-13 01:25:49 -0600

    Works, clean up

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,17 @@
 /** @type {import('vite').UserConfig} */
 
-import { defineConfig, splitVendorChunkPlugin } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [
     react(),
-    splitVendorChunkPlugin(),
   ],
   define: {
     "process.env": {}, // Needed to hack import.meta into React
   },
   server: {
     port: Number(process.env.PORT),
-    fs: {
-      cachedChecks: false,
-    },
   },
   build: {
     sourcemap: true,
