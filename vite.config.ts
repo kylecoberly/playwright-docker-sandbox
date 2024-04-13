0f05326e7fa04e780e404ef3f444a6a82f13f200
Kyle Coberly <kyle.coberly@gmail.com>
2024-04-13 00:38:28 -0600

    Clean up refactor

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,11 @@
 /** @type {import('vite').UserConfig} */
 
-import tsconfigPaths from "vite-tsconfig-paths";
-import path from "node:path";
-import { defineConfig, splitVendorChunkPlugin } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  assetsInclude: ["./public"],
   plugins: [
     react(),
-    splitVendorChunkPlugin(),
-    tsconfigPaths(),
   ],
   define: {
     "process.env": {}, // Needed to hack import.meta into React
@@ -20,11 +15,5 @@ export default defineConfig({
   },
   build: {
     sourcemap: true,
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-      $: path.resolve(__dirname, "./features"),
-    },
   },
 });
